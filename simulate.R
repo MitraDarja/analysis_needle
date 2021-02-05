d9cb4b1e8a3f0061b60d63491c621f8cce1019d4
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-02-05 18:51:17 +0100

    [FIX] Error that only ten percent of experiments have differently expressed genes

--- simulate.R
+++ simulate.R
@@ -20,13 +20,12 @@ for (j in (1:trials)) {
   whichGenes = sample(1:length(fasta),howManyGenes)
   amount = sample(c(0.33,0.5,2,3),howManyGenes,replace = TRUE) # Fold change randomly picked
   fold_changes = matrix(c(rep(1,length(fasta)),rep(1,length(fasta))), nrow=length(fasta))
-  if (j < 11)
-  {
-      for(i in (1:howManyGenes)){
-          out = rbind(out, c(names(fasta[whichGenes[i]]),amount[i]))
-          fold_changes[whichGenes[i],1] = amount[i]
-      }
+
+  for(i in (1:howManyGenes)){
+      out = rbind(out, c(names(fasta[whichGenes[i]]),amount[i]))
+      fold_changes[whichGenes[i],1] = amount[i]
   }
+
 
   write.table(out, file=paste('data/Test_',as.character(j),'.tsv', sep=""),col.names = FALSE, row.names = FALSE, quote=FALSE, sep='\t')
 
