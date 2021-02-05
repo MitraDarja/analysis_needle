23ced1b77ec140380b6974dc46d0408e9ecd19a3
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-02-05 16:02:10 +0100

    [FIX] Change input order and fix logical operators

--- check_titration_monoticuty.py
+++ check_titration_monoticuty.py
@@ -11,8 +11,8 @@ def get_exp_value(line, method):
     elif (method == 2):
         return [float(line.split()[3])]
 
-num_files = int(sys.argv[1])
-method = int(sys.argv[2]) # 0: needle count 1: kallisto 2: salmon
+method = int(sys.argv[1]) # 0: needle count 1: kallisto 2: salmon
+num_files = int(sys.argv[2])
 files = []
 for i in range(3, 3+num_files):
     files.append(sys.argv[i])
@@ -24,36 +24,37 @@ it = 0
 values = {}
 miss = 0
 
-for i in range(1,5):
+for i in range(4):
     values.update({i:{}})
-
-
 
 for file in files:
     with open(file, 'r') as f:
         for line in f:
-            gene = line.split()[0].split('|')[5]
+            gene =  line.split()[0].split('|')[5]
             exp_list = get_exp_value(line, method)
             length = int(line.split('|')[6])
             if gene in values[it]:
-                values[it][gene][letter].append(exp_list[0])
+                if Letter in values[it][gene]:
+                    values[it][gene][Letter].append(exp_list[0])
+                else:
+                    values[it][gene].update({Letter:exp_list})
             else:
-                values[it].update({gene:{letter:exp_list}})
+                values[it].update({gene:{Letter:exp_list}})
+
         iterator += 1
         it += 1
-        if (it == 4) & (iterator < num_needle_files):
+        if (it == 4) & (iterator < num_files):
             Letter = Letters[0]
             Letters = Letters[1:]
             it = 0
 
-
-for it in range(1,5):
+for it in range(4):
     for gene in values[it]:
         gene_expressions = []
         for l in "ACDB": # Not ABCD, because than it is not in the right order
             exps = np.array(values[it][gene][l])
             gene_expressions.append(np.mean(exps, axis = 0))
-        if (!(all([gene_expressions[j] <= gene_expressions[j + 1] for j in range(len(gene_expressions)-1)]) )):#| all(gene_expressions[j] >= gene_expressions[j + 1] for j in range(len(gene_expressions)-1)))):
+        if not (all(gene_expressions[j] <= gene_expressions[j + 1] for j in range(len(gene_expressions)-1))  | all(gene_expressions[j] >= gene_expressions[j + 1] for j in range(len(gene_expressions)-1))):
             miss += 1
 
 print(miss)
