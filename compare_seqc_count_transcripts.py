07225a589aba032c33fa779501d60fd7d0872b99
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-02-05 11:23:14 +0100

    [MISC] Clean up code

--- compare_seqc_count_transcripts.py
+++ compare_seqc_count_transcripts.py
@@ -22,18 +22,14 @@ with open(seqc_file, 'r') as f:
         if line[0] != "E":
             line_table = line.split()
             gene = line_table[1]
+            exp_list_A = [float(x) for x in [line_table[2], line_table[4], line_table[6], line_table[8]]]
+            exp_list_B = [float(x) for x in [line_table[10], line_table[12], line_table[14], line_table[16]]]
+            exp_list_C = [float(x) for x in [line_table[18], line_table[20], line_table[22], line_table[24]]]
+            exp_list_D = [float(x) for x in [line_table[26], line_table[28], line_table[30], line_table[32]]]
             if gene not in seqc_values:
-                exp_list_A = [float(x) for x in [line_table[2], line_table[4], line_table[6], line_table[8]]]
-                exp_list_B = [float(x) for x in [line_table[10], line_table[12], line_table[14], line_table[16]]]
-                exp_list_C = [float(x) for x in [line_table[18], line_table[20], line_table[22], line_table[24]]]
-                exp_list_D = [float(x) for x in [line_table[26], line_table[28], line_table[30], line_table[32]]]
                 dict = {"A": [exp_list_A], "B": [exp_list_B], "C": [exp_list_C],"D": [exp_list_D] }
                 seqc_values.update({gene: dict})
             else:
-                exp_list_A = [float(x) for x in [line_table[2], line_table[4], line_table[6], line_table[8]]]
-                exp_list_B = [float(x) for x in [line_table[10], line_table[12], line_table[14], line_table[16]]]
-                exp_list_C = [float(x) for x in [line_table[18], line_table[20], line_table[22], line_table[24]]]
-                exp_list_D = [float(x) for x in [line_table[26], line_table[28], line_table[30], line_table[32]]]
                 seqc_values[gene]["A"].append(exp_list_A)
                 seqc_values[gene]["B"].append(exp_list_B)
                 seqc_values[gene]["C"].append(exp_list_C)
@@ -59,20 +55,15 @@ for file in files:
     for gene in seqc_values:
         if gene in needle_values:
             exps = np.array(needle_values[gene])
-            if (np.mean(exps, axis = 0) >= 0):
-                needle.append(np.mean(exps, axis = 0) ) # Chisana uses + 0.5
-                exps2 = np.array(seqc_values[gene][Letter])
-                # How Chisanga did it
-                #means = [np.mean(exps2, axis = 1)]
-                #pos_max = means.index(max(means))
-                #seqc.append(np.log2(exps2[pos_max][it]+1)) # /np.mean(gene_lengths[gene], axis = 0)
-                #seqc.append((exps2[pos_max][it]))
-                #seqc.append(np.log2(np.mean(exps2, axis = 0)[it]+1))
-                seqc.append((np.mean(exps2, axis = 0)[it]))
-            else:
-                miss +=1
-            #if (np.mean(exps2, axis = 0)[it] > 0) & (np.mean(exps, axis = 0) == 0):
-                #print(gene, np.mean(exps2, axis = 0)[it], np.mean(exps, axis = 0), Letter, it)
+            needle.append(np.mean(exps, axis = 0) ) # Chisana uses + 0.5
+            exps2 = np.array(seqc_values[gene][Letter])
+            # How Chisanga did it
+            #means = [np.mean(exps2, axis = 1)]
+            #pos_max = means.index(max(means))
+            #seqc.append(np.log2(exps2[pos_max][it]+1)) # /np.mean(gene_lengths[gene], axis = 0)
+            #seqc.append((exps2[pos_max][it]))
+            #seqc.append(np.log2(np.mean(exps2, axis = 0)[it]+1))
+            seqc.append((np.mean(exps2, axis = 0)[it]))
         else:
             miss += 1
 
