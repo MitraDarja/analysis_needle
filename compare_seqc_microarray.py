cc4ad2a8f826ea9e2a2e4f94812e2c541f6cb0ba
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-02-08 16:41:08 +0100

    [MISC] One file for seqc and mircoarray for all methods

--- compare_seqc_microarray.py
+++ compare_seqc_microarray.py
@@ -1,3 +1,7 @@
+# Use:
+# seqc: python3 compare_seqc_microarray.py method(0, 1, 2 for needle count, kallisto or salmon) 0 secq_expression num_files data
+# microrray: python3 compare_seqc_microarray.py method(0, 1, 2 for needle count, kallisto or salmon) 1 secq_expression file with entred2gene_id_info num_files data 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -13,11 +17,21 @@ def get_exp_value(line, method):
         return [float(line.split()[3])]
 
 method = int(sys.argv[1]) # 0: needle count 1: kallisto 2: salmon
-seqc_file = sys.argv[2]
-num_files = int(sys.argv[3])
-files = []
-for i in range(4, 4+num_files):
-    files.append(sys.argv[i])
+microrray = int(sys.argv[2]) # 0: seqc 1: microarrayy
+j = 3
+if microrray:
+    seqc_file = sys.argv[j]
+    seqc_file2 = sys.argv[j+1]
+    num_files = int(sys.argv[j+2])
+    files = []
+    for i in range(j+3, j+3+num_files):
+        files.append(sys.argv[i])
+else:
+    seqc_file = sys.argv[j]
+    num_files = int(sys.argv[j+1])
+    files = []
+    for i in range(j+2, j+2+num_files):
+        files.append(sys.argv[i])
 
 seqc_values = {}
 iterator = 0
@@ -27,11 +41,27 @@ pearson = []
 spearman = []
 it = 0
 miss = 0
+
+# Get Gene ids for microarray data
+gene_ids = {}
+if microrray:
+    with open(seqc_file2, 'r') as f:
+        for line in f:
+            #print(line)
+            if line[0] != "q":
+                line_table = line.split()
+                gene = line_table[1]
+                if gene not in gene_ids:
+                    gene_ids.update({line_table[0]:gene})
+
 with open(seqc_file, 'r') as f:
     for line in f:
         if line[0] != "E":
             line_table = line.split()
-            gene = line_table[1]
+            if microrray:
+                gene =  gene_ids[line_table[0]]
+            else:
+                gene = line_table[1]
             exp_list_A = [float(x) for x in [line_table[2], line_table[4], line_table[6], line_table[8]]]
             exp_list_B = [float(x) for x in [line_table[10], line_table[12], line_table[14], line_table[16]]]
             exp_list_C = [float(x) for x in [line_table[18], line_table[20], line_table[22], line_table[24]]]
