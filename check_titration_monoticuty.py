a333fbae731ccc2712db60818a000b44bb41607e
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-02-08 20:27:45 +0100

    [FEATURE] Add normalization per file to needle

--- check_titration_monoticuty.py
+++ check_titration_monoticuty.py
@@ -30,6 +30,7 @@ for i in range(4):
     values.update({i:{}})
 
 for file in files:
+    gene_lengths = {}
     with open(file, 'r') as f:
         for line in f:
             if (line[0] == "N") | (line[0] == "t"):
@@ -40,10 +41,13 @@ for file in files:
             if gene in values[it]:
                 if Letter in values[it][gene]:
                     values[it][gene][Letter].append(exp_list[0])
+                    gene_lengths[gene].append(length)
                 else:
                     values[it][gene].update({Letter:exp_list})
+                    gene_lengths.update({gene:[length]})
             else:
                 values[it].update({gene:{Letter:exp_list}})
+                gene_lengths.update({gene:[length]})
 
         iterator += 1
         it += 1
@@ -51,6 +55,17 @@ for file in files:
             Letter = Letters[0]
             Letters = Letters[1:]
             it = 0
+
+norm_all = {}
+for i in range(4):
+    norm_all.update({i:{"A":[0,0,0,0], "B":[0,0,0,0], "C":[0,0,0,0], "D":[0,0,0,0]}})
+for it in range(4):
+    for gene in values[it]:
+        gene_count +=1
+        gene_expressions = []
+        for l in "ACDB": # Not ABCD, because than it is not in the right order
+            exps = np.array(values[it][gene][l])
+            norm_all[it][l] += np.mean(exps, axis = 0)/np.mean(gene_lengths[gene])
 
 expected_fold_change_c_d  = []
 fold_change_c_d = []
@@ -60,7 +75,10 @@ for it in range(4):
         gene_expressions = []
         for l in "ACDB": # Not ABCD, because than it is not in the right order
             exps = np.array(values[it][gene][l])
-            gene_expressions.append(np.mean(exps, axis = 0))
+            if method == 0:
+                gene_expressions.append(np.mean(exps, axis = 0)/np.mean(norm_all[it][l],axis=0))
+            else:
+                gene_expressions.append(np.mean(exps, axis = 0))
         if not (all(gene_expressions[j] <= gene_expressions[j + 1] for j in range(len(gene_expressions)-1))  | all(gene_expressions[j] >= gene_expressions[j + 1] for j in range(len(gene_expressions)-1))):
             miss += 1
         if all(gene_expressions[j] > 0 for j in range(len(gene_expressions))):
