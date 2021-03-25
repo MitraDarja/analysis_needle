69a216002537bddc360796c84228174525b961c1
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-03-25 16:55:57 +0100

    [MISC] Clean up code.

--- check_titration_monotonicity.py
+++ check_titration_monotonicity.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from scipy import stats
@@ -56,46 +55,20 @@ for file in files:
             Letters = Letters[1:]
             it = 0
 
-# Normalization
-norm_all = {}
-norm_all.update({"A":[0,0,0,0], "B":[0,0,0,0], "C":[0,0,0,0], "D":[0,0,0,0]})
-for it in range(4):
-    for gene in values[it]:
-        gene_count +=1
-        for l in "ABCD":
-            exps = np.array(values[it][gene][l])
-            if (method == 0):
-                norm_all[l][it] += np.mean(exps, axis = 0)
-            else:
-                norm_all[l][it] += np.mean(exps, axis = 0)/np.mean(gene_lengths[gene])
-
-for i in range(4):
-    for letter in "ABCD":
-        norm_all[letter][i] = norm_all[letter][i]/1000000.0
-
 # Calculate MSE
-print(norm_all)
-expected_fold_change_c_d  = []
-fold_change_a_b = []
-fold_change_c_d = []
 error = []
 for it in range(4):
     for gene in values[it]:
         gene_expressions = []
-        if 1:#len(values[it][gene][l]) > 0:
-            gene_count +=1
-            for letter in "ABCD":
-                exps = np.array(values[it][gene][letter]/norm_all[it][letter])
-                gene_expressions.append(np.mean(exps, axis = 0))
-            expected_fold_change_c_d.append(np.log2(1+float(gene_expressions[0] + (3*gene_expressions[3]))/((gene_expressions[3] + 1 + (3*gene_expressions[0]))))) # A+3B/3A+B = B/A
-            fold_change_a_b.append(np.log2(1+float(gene_expressions[3])/(gene_expressions[0]+1)))
-            fold_change_c_d.append(np.log2(1+float(gene_expressions[2])/(gene_expressions[1]+1)))
-            error.append((expected_fold_change_c_d[-1]-fold_change_c_d[-1])* (expected_fold_change_c_d[-1]-fold_change_c_d[-1]))
-
-
+        gene_count +=1
+        for letter in "ABCD":
+            exps = np.array(values[it][gene][letter])
+            gene_expressions.append(np.mean(exps, axis = 0))
+        # + 1 for dealing with zeros
+        expected_fold_change_c_d = np.log2(1+float(gene_expressions[0] + (3*gene_expressions[3]))/((gene_expressions[3] + 1 + (3*gene_expressions[0])))) # A+3B/3A+B = B/A
+        fold_change_a_b = np.log2(1+float(gene_expressions[3])/(gene_expressions[0]+1))
+        fold_change_c_d = np.log2(1+float(gene_expressions[2])/(gene_expressions[1]+1))
+        error.append((expected_fold_change_c_d-fold_change_c_d)* (expected_fold_change_c_d-fold_change_c_d))
 
 print(gene_count)
 print(np.mean(error), len(error))
-#plt.plot(expected_fold_change_c_d, fold_change_c_d, color="red")
-plt.plot(fold_change_a_b, fold_change_c_d, 'o', color = "black")
-plt.savefig("check_titration_"+str(method)+".png")
