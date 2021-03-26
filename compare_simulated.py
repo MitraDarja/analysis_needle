b98e75787f266c5c7e18ce95c267fedd16a9c15a
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-03-26 11:23:51 +0100

    [MISC] Only consider fold_changes where both transcripts are expressed more than 0.

--- compare_simulated.py
+++ compare_simulated.py
@@ -9,16 +9,15 @@ import sys
 from scipy import stats
 
 # Here two experiments are compared, so a normalization like TPM should be used. kallisto and salmon provide a TPM
-# normalization and return the TPM value for every transcript, but the transcript file provided contained more
-# transcript than we will check during one comparison, therefore for kallisto and salmon not the TPM value is considered
-# but the estimated counts. A TPM-normalization is done for all three methods later in this script.
+# normalization and return the TPM value for every transcript, this value is used here.
+# A TPM-normalization is done for Needle later in this script.
 def get_exp_value(line, method):
     if (method == 0):
         return [int(x) for x in line.split()[1:]][0]
     elif (method == 1):
-        return  float(line.split()[3])
+        return  float(line.split()[4])
     elif (method == 2):
-        return float(line.split()[4])
+        return float(line.split()[3])
 
 method = int(sys.argv[1]) # 0: needle count 1: kallisto 2: salmon
 j = 2
@@ -56,10 +55,6 @@ for i in range(0, len(files), 2):
                 values_1.update({transcript:exp_list})
                 if (method == 0):
                     per_million_1 += exp_list
-                # Kallisto and salmon need transcript length correction, for needle done by taking median
-                else:
-                    per_million_1 += exp_list/int(line.split('|')[6])
-
 
     with open(files[i+1], 'r') as f:
         for line in f:
@@ -69,13 +64,15 @@ for i in range(0, len(files), 2):
                 values_2.update({transcript:exp_list})
                 if (method == 0):
                     per_million_2 += exp_list
-                # Kallisto and salmon need transcript length correction, for needle done by taking median
-                else:
-                    per_million_2 += exp_list/int(line.split('|')[6])
+
 
     # TPM factor for both files
-    per_million_1 = per_million_1/1000000.0
-    per_million_2 = per_million_2/1000000.0
+    if (method == 0)
+        per_million_1 = per_million_1/1000000.0
+        per_million_2 = per_million_2/1000000.0
+    else:
+        per_million_1 = 1
+        per_million_2 = 1
     for transcript in expected_values:
         if (transcript in values_1) & (transcript in values_2):
             # Rounding of values for kallisto, which has sometimes really small values, which lead to
@@ -90,12 +87,22 @@ for i in range(0, len(files), 2):
                 if ((fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript])) > max:
                     max = (fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript])
                     max_transcript = transcript
+            elif (values_1[transcript] > 0):
+                fold_change = values_1[transcript]
+                errors.append((fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript]))
+            elif (values_2[transcript] > 0):
+                fold_change = values_2[transcript]
+                errors.append((fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript]))
+            else:
+                # If transcript is not found in both experiments, add expected value as error
+                # This is necessary because salmon does not give an answer for some transcripts for some reason
+                errors.append((expected_values[transcript]) * (expected_values[transcript]))
         else:
             if (transcript in values_1):
-                fold_change = values_1[transcript] + 1
+                fold_change = values_1[transcript]
                 errors.append((fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript]))
             elif (transcript in values_2):
-                fold_change = values_2[transcript]+ 1
+                fold_change = values_2[transcript]
                 errors.append((fold_change-expected_values[transcript]) * (fold_change-expected_values[transcript]))
             else:
                 # If transcript is not found in both experiments, add expected value as error
