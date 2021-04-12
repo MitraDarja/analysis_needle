895292844514414f7ccb5d148ea347eece6e61d8
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-04-12 15:18:46 +0200

    [MISC] Add estimate to check titration monotoncity.

--- check_titration_monotonicity.py
+++ check_titration_monotonicity.py
@@ -14,9 +14,6 @@ def get_exp_value(line, method):
 
 method = int(sys.argv[1]) # 0: needle count 1: kallisto 2: salmon
 num_files = int(sys.argv[2])
-files = []
-for i in range(3, 3+num_files):
-    files.append(sys.argv[i])
 
 iterator = 0
 Letter = "A"
@@ -28,33 +25,54 @@ gene_count = 0
 for i in range(4):
     values.update({i:{}})
 
-# Read in values for every file
-for file in files:
-    gene_lengths = {}
-    with open(file, 'r') as f:
-        for line in f:
-            if (line[0] == "N") | (line[0] == "t"):
-                continue
-            gene =  line.split()[0].split('|')[5]
-            exp_list = get_exp_value(line, method)
-            length = int(line.split('|')[6])
-            if gene in values[it]:
-                if Letter in values[it][gene]:
-                    values[it][gene][Letter].append(exp_list[0])
-                    gene_lengths[gene].append(length)
-                else:
-                    values[it][gene].update({Letter:exp_list})
-                    gene_lengths.update({gene:[length]})
-            else:
-                values[it].update({gene:{Letter:exp_list}})
-                gene_lengths.update({gene:[length]})
+if (method != 3):
+    files = []
+    for i in range(3, 3+num_files):
+        files.append(sys.argv[i])
 
-        iterator += 1
-        it += 1
-        if (it == 4) & (iterator < num_files):
-            Letter = Letters[0]
-            Letters = Letters[1:]
-            it = 0
+    # Read in values for every file
+    for file in files:
+        gene_lengths = {}
+        with open(file, 'r') as f:
+            for line in f:
+                if (line[0] == "N") | (line[0] == "t"):
+                    continue
+                gene =  line.split()[0].split('|')[0]
+                exp_list = get_exp_value(line, method)
+                length = int(line.split('|')[6])
+                if gene in values[it]:
+                    if Letter in values[it][gene]:
+                        values[it][gene][Letter].append(exp_list[0])
+                        gene_lengths[gene].append(length)
+                    else:
+                        values[it][gene].update({Letter:exp_list})
+                        gene_lengths.update({gene:[length]})
+                else:
+                    values[it].update({gene:{Letter:exp_list}})
+                    gene_lengths.update({gene:[length]})
+
+            iterator += 1
+            it += 1
+            if (it == 4) & (iterator < num_files):
+                Letter = Letters[0]
+                Letters = Letters[1:]
+                it = 0
+else:
+    values = {}
+    with open(sys.argv[3], 'r') as f:
+        for line in f:
+            gene = line.split()[0].split('|')[5]
+            exp_list = [int(x) for x in line.split()[1:]]
+
+            if gene not in values:
+                dict = {"A": [exp_list[0:4]], "B": [exp_list[4:8]], "C": [exp_list[8:12]],"D": [exp_list[12:16]] }
+                values.update({gene: dict})
+
+            else:
+                values[gene]["A"].append(exp_list[0:4])
+                values[gene]["B"].append(exp_list[4:8])
+                values[gene]["C"].append(exp_list[8:12])
+                values[gene]["D"].append(exp_list[12:16])
 
 # Normalization, a normalization should be performed because different experiments are compared to each other
 norm_all = {}
@@ -80,7 +98,7 @@ for it in range(4):
         gene_count +=1
         for letter in "ABCD":
             exps = np.array(values[it][gene][letter])
-            gene_expressions.append(np.mean(exps, axis = 0)/norm_all[letter][it])
+            gene_expressions.append(exps/norm_all[letter][it])
         # + 1 for dealing with zeros
         expected_fold_change_c_d = np.log2(1+float(gene_expressions[0] + (3*gene_expressions[3]))/((gene_expressions[3] + 1 + (3*gene_expressions[0])))) # A+3B/3A+B = B/A
         fold_change_a_b = np.log2(1+float(gene_expressions[3])/(gene_expressions[0]+1))
