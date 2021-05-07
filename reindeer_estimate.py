d55d2c9a5a4942e3fd518a093a0a4e91cd16f79f
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-05-07 17:59:30 +0200

    [DOC] Add doc to reinder_estimate.py

--- reindeer_estimate.py
+++ reindeer_estimate.py
@@ -6,6 +6,7 @@ fasta_file = sys.argv[2]
 out_file = sys.argv[3]
 
 genes = {}
+# Necessary, so the complete fasta id can be stored
 with open(fasta_file, 'r') as f:
     for line in f:
         if (line[0] == ">"):
@@ -13,27 +14,38 @@ with open(fasta_file, 'r') as f:
 
 with open(reindeer_file, 'r') as f:
     with open(out_file, 'w') as o:
+        # One line gives the result for every dataset
         for line in f:
             estimations = []
             transcript_name = line.split('|')[0]
             if transcript_name in genes:
+                # Each dataset result is divided by a ' ', so results contains all results for all datasets.
+                # First entry in a line is the gene, so can be ignored.
                 results = line.split()[1:]
                 for res in results:
-                    kmers_occ = []
+                    kmers_occ = [] # store the occurrences of the k-mers
+                    # * means not found
                     if (res != '*'):
+                        # Different counts are divided by ','
                          different_counts = res.split(',')
                          for counts in different_counts:
                              occurrences = []
+                             # Entry looks like X-Y:Z is, where X and are the position of the k-mers and Z the count value
+                             # kmers = Y-X, so number of k-mers with that count value
                              kmers = int(counts.split(':')[0].split('-')[1]) - int(counts.split(':')[0].split('-')[0])
                              occ = 0
+                             # occ_s is the occurence (Z above), can be * if not found
                              occ_s = counts.split(':')[1]
                              if (occ_s != '*'):
                                  occ = int(occ_s)
+                            # Add all occurences to kmers_occ
                              occurrences = [occ] * kmers
                              kmers_occ += occurrences
                     if (kmers_occ == []):
                         kmers_occ = [0]
+                    # Take median of all k-mer counts as espression value for one dataset
                     estimations.append(int(statistics.median(kmers_occ)))
+                # Write out the expression values
                 o.write(genes[transcript_name])
                 o.write('\t')
                 for e in estimations:
