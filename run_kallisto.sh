2fea33b05e2d1bdca6838ccbe2fe09f317805dda
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-05-10 17:34:44 +0200

    [MISC] Delete not needed files.

--- run_kallisto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for letter in "A" "B" "C" "D"; do
-    for i in 1 2 3 4; do
-        ./kallisto quant -i gencode.v36.index -o SEQC2012-ILM-AGR-${letter}-${i}/ ../Chisanga_data/fqs/SEQC2012-ILM-AGR-${letter}-${i}_R1.fastq.gz ../Chisanga_data/fqs/SEQC2012-ILM-AGR-${letter}-${i}_R2.fastq.gz -g ../data/gencode.v36.annotation.gtf
-    done
-done
--- run_salmon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for letter in "A" "B" "C" "D"; do
-    for i in 1 2 3 4; do
-        ./src/salmon quant -i gencode.v36.index/ -l ISF -1 ../../Chisanga_data/fqs/SEQC2012-ILM-AGR-${letter}-${i}_R1.fastq.gz -2 ../../Chisanga_data/fqs/SEQC2012-ILM-AGR-${letter}-${i}_R2.fastq.gz -o out/SEQC2012-ILM-AGR-${letter}-${i}.out -g ../../needle/gencode.v36.annotation.gtf
-    done
-done
